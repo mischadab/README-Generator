8f7df877f5db2e6ab7566b76491b856571c4c8ce
mischadab <mischadabrowski@gmail.com>
2021-02-11 16:44:30 -0500

    added table of contents to generate markdown

--- READMEE.md
+++ READMEE.md
@@ -1,10 +1,52 @@
-# weee
+# fff
   ![GitHub License](https://img.shields.io/badge/license-MIT-blue.svg)
 
   ## Table of Contents
 
+  * [Description](#description)
+
+  * [Installation](#installation)
+
+  * [Usage](#usage)
+
   
+* [License](#license)
+
+  * [Contributing](#coontributing)
+
+  * [Tests](#tests)
+
+  * [Questions](#questions)
+
+
+
+  ## Description
+  undefined
+  
+
   ## Installation
-  npm install
+  ```
+  dvc
+  ```
+
+
+  ## Usage
+  cxv
+
+
+  ## License
+
+
+  ## Contributing
+  fgg
+
   
+  ## Tests
+  ```
+  npm test
+  ```
  
+
+  ## Questions
+  If you have any questions about this repo, contact me at fff
+  See my other projects at fgff
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,43 +1,81 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if( license !== 'None' ){
-    return `![GitHub License](https://img.shields.io/badge/license-${license}-blue.svg)`
-  }
-  return ""
+	if (license !== 'None') {
+		return `![GitHub License](https://img.shields.io/badge/license-${license}-blue.svg)`;
+	}
+	return '';
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if( license !== 'None' ){
-    return `\n* [License](#license)`;
-  }
-  return ""
+	if (license !== 'None') {
+		return `\n* [License](#license)`;
+	}
+	return '';
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  return `## License`
-} 
+	return `## License`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(answer) {
-  return `# ${answer.title}
+	return `# ${answer.title}
   ${renderLicenseBadge(answer.license)}
 
   ## Table of Contents
 
+  * [Description](#description)
+
+  * [Installation](#installation)
+
+  * [Usage](#usage)
+
+  ${renderLicenseLink(answer.license)}
+
+  * [Contributing](#coontributing)
+
+  * [Tests](#tests)
+
+  * [Questions](#questions)
+
+
+
+  ## Description
+  ${answer.descrption}
   
+
   ## Installation
+  \`\`\`
   ${answer.installation}
+  \`\`\`
+
+
+  ## Usage
+  ${answer.usage}
+
+
+  ${renderLicenseSection(answer.license)}
+
+
+  ## Contributing
+  ${answer.contributions}
+
   
+  ## Tests
+  \`\`\`
+  ${answer.test}
+  \`\`\`
  
+
+  ## Questions
+  If you have any questions about this repo, contact me at ${answer.email}
+  See my other projects at ${answer.github}
 `;
 }
 
 module.exports = generateMarkdown;
-
-
-
